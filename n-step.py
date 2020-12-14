1609ab6f0995188071c83eead4ce39b6bbc7dcd7
MaiteFlores <33408663+MaiteFlores@users.noreply.github.com>
2020-12-13 23:42:48 -0500

    Update n-step.py

--- n-step.py
+++ n-step.py
@@ -32,7 +32,6 @@ class Maze(object):
         self.goal = (x-1,y-1)
         self.empty =[(r,c) for r in range(x) for c in range(y) if self._maze[r,c] == 1.0]
         self.border = [[(r,c) for r in range(x) for c in range(y) if self._maze[r,c] == 0.0]]
-        #self.empty.remove(self.goal)
         #self.visited_square = []
         self.restart(runner)
 
@@ -81,7 +80,6 @@ class Maze(object):
     def moves(self, decision):
         self.change_state(decision)
         score = self.reward()
-        # maze_state = self.check_state()
         self.final_reward += score
         check_is_over = self.is_over()
         return score, check_is_over
@@ -107,26 +105,10 @@ class Maze(object):
             return 'Lost'
         runner_xcor,runner_ycor, mode = self.state
         x_axis,y_axis = self._maze.shape
-        if runner_xcor == x_axis-1 and runner_ycor == y_axis-1:
+        if (runner_xcor,runner_ycor) ==  self.goal:
             return 'Winner'
         return 'Playing'
         
-        
-    # creates the environment 
-    def create(self):
-        x, y = self._maze.shape
-        pic = np.copy(self._maze)
-
-        for i in range (x):
-            for j in range (y):
-                if pic[i,j] > 0.0:
-                    #print(x)
-                    pic[i,j] = 1.0
-        #place runner
-        run_x,run_y,running = self.state
-        pic[run_x,run_y] = player
-
-        return pic
 
     def check_move(self,cell = NONE):
 
@@ -346,56 +328,3 @@ get_shortest_path(gen)
 
 
 
-
-##def get_next_action(current_row_index, current_column_index, epsilon):
-##  #if a randomly chosen value between 0 and 1 is less than epsilon, 
-##  #then choose the most promising value from the Q-table for this state.
-##  if np.random.random() < epsilon:
-##    return np.argmax(q_vals[current_row_index, current_column_index])
-##  else: #choose a random action
-##    return random.choice(dif_actions)
-##
-##def get_shortest_path(maze):
-##  #return immediately if this is an invalid starting location
-##  cur_x,cur_y, mode = maze.state
-##  x,y = maze._maze.shape 
-##  if cur_x == x-1 and cur_y == y-1:
-##    is_terminal_state = True
-##    return []
-##  else: #if this is a 'legal' starting location
-##    #current_row_index, current_column_index = start_row_index, start_column_index
-##    is_terminal_state = False
-##    shortest_path = []
-##    shortest_path.append([cur_x, cur_y])
-##    #continue moving along the path until we reach the goal (i.e., the item packaging location)
-##    while not is_terminal_state:
-##      #get the best action to take
-##      action_index = get_next_action(cur_x, cur_y, 1.)
-##      #move to the next location on the path, and add the new location to the list
-##      #cur_x, cur_y = get_next_location(current_row_index, current_column_index, action_index)
-##      maze.moves(action_index)
-##      cur_x,cur_y, mode = maze.state
-##      shortest_path.append([cur_x, cur_y])
-##    return shortest_path
-    
-     
-
-#train_please(show)
-#get_shortest_path(maze)
-#pic, reward, game_over = show.moves(2)
-##show.moves(3)
-##show.moves(3)
-##show.moves(3)
-##show.moves(2)
-##show.moves(1)
-#print("reward=", reward)
-##for i in range(300):
-##    actions = random.choice(dif_actions)
-##    #print(actions)
-##    show.moves(actions)
-####show.moves(0)
-##create_maze(show)
-
-
-
-    
